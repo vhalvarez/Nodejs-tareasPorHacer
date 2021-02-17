8fecf1b377ad612c1a9473a6a262a5c51886c8df
vhalvarez <alvarez.programmer@gmail.com>
2021-02-17 08:56:37 -0400

    Creamos el menu de la aplicación para que se ejecute de forma infinita

---
+++ app.js
@@ -0,0 +1,19 @@
+require('colors')
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes')
+
+console.clear()
+
+const main = async () => {
+    let opt = ''
+
+    do {
+        opt = await mostrarMenu()
+
+        console.log({ opt })
+
+        if (opt !== '0') await pausa()
+    } while (opt !== '0')
+}
+
+main()
