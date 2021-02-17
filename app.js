c1928029c444095b4acd18e39f009dcca63dbdff
vhalvarez <alvarez.programmer@gmail.com>
2021-02-17 09:44:12 -0400

    Logica para el manejo de las tareas por hacer

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 require('colors')
 const { inquirerMenu, pausa } = require('./helpers/inquirer')
+const Tarea = require('./models/tarea')
+const Tareas = require('./models/tareas')
 console.clear()
 
 const main = async () => {
     let opt
 
     do {
-        opt = await inquirerMenu()
+        // opt = await inquirerMenu()
 
-        await pausa()
+        const tareas = new Tareas()
+        const tarea = new Tarea('Comprar comida')
+
+        tareas._listado[tarea.id] = tarea
+
+        console.log(tareas)
         
+        await pausa()
     } while (opt !== 0)
 }
-
 
 main()
